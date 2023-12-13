cd02d727eb7658e1a2054267dd0ab5487ead757b
Shaurya Aher <shauryaaher1200@gmail.com>
2023-12-13 09:35:04 +0530

    Update PrimePalindrome.java

--- constructors/PrimePalindrome.java
+++ constructors/PrimePalindrome.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class PrimePalindrome {
-    PrimePalindrome() {
-        int se = 0;
-        int s = 0;
-        
+    private boolean isPrime;
+    private boolean isPalindrome;
+    PrimePalindrome(int num) {
+        isPrime = true;
+        isPalindrome = false;
+        for (int i = 2; i < num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        String s = Integer.toString(num);
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        int val = Integer.parseInt(sb.toString());
+        if (val == num) {
+            isPalindrome = true;
+        }
     }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        PrimePalindrome inst = new PrimePalindrome(n);
+        if (inst.isPrime && inst.isPalindrome == true) {
+            System.out.print("This is a prime palindrome number.");
+        } else {
+            System.out.print("This is not a prime palindrome number");
+        }
     }
 }
